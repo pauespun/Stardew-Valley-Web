1eb461c00fd36fa39f5c9d273496cd1fc9e76aa3
pauespun <pau.espun@estudiantat.upc.edu>
2026-06-02 16:45:07 +0200

    configs

--- stardew_valley/stardew_valley/settings.py
+++ stardew_valley/stardew_valley/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -75,14 +78,13 @@ WSGI_APPLICATION = 'stardew_valley.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'est_e7126609',
-        'USER': 'est_e7126609',
-        'PASSWORD': 'dB.e7126609',
-        'HOST': 'ubiwan.epsevg.upc.edu',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
