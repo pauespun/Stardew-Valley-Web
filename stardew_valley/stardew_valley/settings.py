37ae3f62c8e6f987a0c0b7c93517674c1ea1ff3a
pauespun <pau.espun@estudiantat.upc.edu>
2026-06-02 16:29:39 +0200

    setup

--- stardew_valley/stardew_valley/settings.py
+++ stardew_valley/stardew_valley/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'stardew_valley.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'est_e7126609',
+        'USER': 'est_e7126609',
+        'PASSWORD': 'dB.e7126609',
+        'HOST': 'ubiwan.epsevg.upc.edu',
+        'PORT': '5432',
     }
 }
 
